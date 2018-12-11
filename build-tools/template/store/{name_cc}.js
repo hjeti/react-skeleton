7a91b8265578c2c5f0a324dc2f3e1c6de09aba08
Hendrik-Jan de Harder <hendrik-jan@mediamonks.com>
2018-12-11 00:52:17 -0300

    add seng generator store template

---
+++ build-tools/template/store/{name_cc}.js
@@ -0,0 +1,17 @@
+import { handleActions } from 'redux-actions';
+
+// Actions
+const namespace = '{{name_cc}}';
+
+// Reducer
+const initialState = {};
+
+const {{name_cc}}Reducer = handleActions({}, initialState);
+
+export default {{name_cc}}Reducer;
+
+// Action creators
+
+// Thunks
+
+// Selectors
